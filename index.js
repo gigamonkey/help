a3e31314aff4a3016f085691950fba2367688c92
Peter Seibel <peter@gigamonkeys.com>
2022-08-03 16:26:55 -0700

    Prettying

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-app.listen(3000, () => console.log(`App is listening on port 3000!`))
+app.listen(3000, () => console.log(`App is listening on port 3000!`));
