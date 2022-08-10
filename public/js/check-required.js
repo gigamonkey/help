c620121ae74abf312572a7d5b806cc57aea6d9d5
Peter Seibel <peter@gigamonkeys.com>
2022-08-09 20:09:08 -0700

    Tidy up html and templates a little bit.

--- public/js/check-required.js
+++ public/js/check-required.js
@@ -1,9 +1,9 @@
-import { $, $$ } from '/js/modules/common.js';
+import { $, $$ } from './modules/common.js';
 
 const checkReady = () => {
   $('#submit').disabled = ![...$$('.required')].every((e) => e.value);
 };
 
-$$('.required').forEach((e) => (e.oninput = checkReady));
+$$('.required').forEach((e) => { e.oninput = checkReady; });
 
 checkReady();
