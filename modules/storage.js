0d62ede662f20c36a2d5c25f71b5c9a32e5184fd
Peter Seibel <peter@gigamonkeys.com>
2022-08-14 14:24:03 -0700

    Add Mr. Albinson.

--- modules/storage.js
+++ modules/storage.js
@@ -8,11 +8,13 @@ const DIRNAME = url.fileURLToPath(new URL('.', import.meta.url));
 const DDL = fs.readFileSync(path.resolve(DIRNAME, 'schema.sql'), 'utf-8');
 
 const ADMINS = {
+  'mattalbinson@berkeley.net': true,
   'peterseibel@berkeley.net': true,
   'shoshanaokeefe@berkeley.net': true,
 };
 
 const OTHER_NAMES = {
+  'mattalbinson@berkeley.net': 'Mr. Albinson',
   'peterseibel@berkeley.net': 'Mr. Seibel',
   'shoshanaokeefe@berkeley.net': 'Ms. O’Keefe',
 };
